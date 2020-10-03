d3a1767d49a76b7ace3525e7810e95a913466118
AlexMelenchon <melenchonatona@gmail.com>
2020-10-03 11:51:21 +0200

    0.0.16 - Now we can get inputs for ImGio

--- CollyraEngine/M_Input.cpp
+++ CollyraEngine/M_Input.cpp
@@ -2,6 +2,10 @@
 #include "Application.h"
 #include "M_Input.h"
 
+#include "imgui/imgui.h"
+#include "imgui/imgui_internal.h"
+#include "imgui/imgui_impl_sdl.h"
+#include "imgui/imgui_impl_opengl3.h"
 
 #define MAX_KEYS 300
 
@@ -89,7 +93,9 @@ updateStatus M_Input::PreUpdate(float dt)
 	SDL_Event e;
 	while(SDL_PollEvent(&e))
 	{
-		
+	
+		ImGui_ImplSDL2_ProcessEvent(&e);
+
 		switch(e.type)
 		{
 			case SDL_MOUSEWHEEL:
