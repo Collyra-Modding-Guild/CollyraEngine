5e72242e9e622d59ca163b2bcc4b8bbe3886bc14
AlexMelenchon <melenchonatona@gmail.com>
2020-10-15 19:50:50 +0200

    0.0.36b - Fixed merge in Render3D PostUpdate
    - Accidently deleted BehinDrawMode()

--- CollyraEngine/M_Renderer3D.cpp
+++ CollyraEngine/M_Renderer3D.cpp
@@ -141,14 +141,6 @@ bool M_Renderer3D::Awake()
 		lights[1].Active(true);
 	}
 
-
-	//CCube* c = new CCube(10, 10, 10);
-
-	//SSphere* s = new SSphere(4, 20, 40);
-
-	//primitives.push_back(c);
-	//primitives.push_back(s);
-
 	// Projection matrix for
 	OnResize();
 
@@ -200,21 +192,7 @@ updateStatus M_Renderer3D::PostUpdate(float dt)
 	updateStatus ret = UPDATE_CONTINUE;
 
 
-	/*SSphere s(1.0f, 36, 18);
-	s.InnerRender();
-	
-	Pyramid pyr(1, 1, 1);
-	pyr.InnerRender();*/
-
-
-
-	//Grind + Axis
-	CPlane p(0, 1, 0, 0);
-	p.axis = true;
-	p.Render();
 	BeginDrawMode();
-
-
 
 	//Debug Render
 	if (App->IsDebugModeOn() == true)
