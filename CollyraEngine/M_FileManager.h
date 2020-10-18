956003f83cb0e314f5469abb72c3caa43bc748a3
Aaron Guerrero Cruz <47571598+AaronGCProg@users.noreply.github.com>
2020-10-18 19:19:31 +0200

    0.0.42 PhysFS - File System improved

--- CollyraEngine/M_FileManager.h
+++ CollyraEngine/M_FileManager.h
@@ -11,6 +11,15 @@
 #include <vector>
 #include <string>
 
+struct SDL_RWops;
+int close_sdl_rwops(SDL_RWops* rw);
+
+struct aiFileIO;
+
+//struct BASS_FILEPROCS;
+class Config;
+struct PathNode;
+
 class M_FileManager : public Module
 {
 public:
@@ -22,19 +31,47 @@ public:
 	bool Awake();
 
 	// Called before quitting
-	bool CleanUp();
+	bool CleanUp() override;
+
+	void CreateLibraryDirectories();
 
 	// Return the bytes of a PhysFS filehandle
-	uint Load(const char* path, char** buffer) const;
+	//uint Load(const char* path, char** buffer) const;
 
 	// Allows you to use pointers to memory instead of files or things such as images or samples
 	SDL_RWops* Load(const char* path);
 
-	bool M_FileManager::HasExtension(const char* path) const;
-	bool M_FileManager::HasExtension(const char* path, std::string extension) const;
-	bool M_FileManager::HasExtension(const char* path, std::vector<std::string> extensions) const;
+	// Utility functions
+	bool AddPath(const char* path_or_zip);
+	bool Exists(const char* file) const;
+	bool CreateDir(const char* dir);
+	bool IsDirectory(const char* file) const;
+	const char* GetWriteDir() const;
+	void DiscoverFiles(const char* directory, std::vector<std::string>& file_list, std::vector<std::string>& dir_list) const;
+	void GetAllFilesWithExtension(const char* directory, const char* extension, std::vector<std::string>& file_list) const;
+	PathNode GetAllFiles(const char* directory, std::vector<std::string>* filter_ext = nullptr, std::vector<std::string>* ignore_ext = nullptr) const;
+	void GetRealDir(const char* path, std::string& output) const;
+	std::string GetPathRelativeToAssets(const char* originalPath) const;
 
-	void M_FileManager::SplitFilePath(const char* full_path, std::string* path, std::string* file, std::string* extension) const;
+	bool HasExtension(const char* path) const;
+	bool HasExtension(const char* path, std::string extension) const;
+	bool HasExtension(const char* path, std::vector<std::string> extensions) const;
+
+	std::string NormalizePath(const char* path) const;
+	void SplitFilePath(const char* full_path, std::string* path, std::string* file = nullptr, std::string* extension = nullptr) const;
+
+	// Open for Read/Write
+	unsigned int Load(const char* path, const char* file, char** buffer) const;
+	unsigned int Load(const char* file, char** buffer) const;
+
+	bool DuplicateFile(const char* file, const char* dstFolder, std::string& relativePath);
+	bool DuplicateFile(const char* srcFile, const char* dstFile);
+
+	unsigned int Save(const char* file, const void* buffer, unsigned int size, bool append = false) const;
+	bool Remove(const char* file);
+
+	uint64 GetLastModTime(const char* filename);
+	std::string GetUniqueName(const char* path, const char* name) const;
 
 private:
 	int CheckPath(const char*);
