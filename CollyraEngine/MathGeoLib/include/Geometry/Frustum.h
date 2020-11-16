540a4bb93aa44bfc790bcf0f9de1fb485b06f220
Aaron Guerrero Cruz <47571598+AaronGCProg@users.noreply.github.com>
2020-11-16 18:04:23 +0100

    0.1.13 - Contains via Vertices added. Frustum Improved

--- CollyraEngine/MathGeoLib/include/Geometry/Frustum.h
+++ CollyraEngine/MathGeoLib/include/Geometry/Frustum.h
@@ -378,6 +378,8 @@ public:
 	bool Contains(const Frustum &frustum) const;
 	bool Contains(const Polyhedron &polyhedron) const;
 
+	bool ContainsAABBVertices(const AABB &AABB) const;
+
 	/// Computes the closest point inside this Frustum to the given point.
 	/** If the target point lies inside this Frustum, then that point is returned.
 		@see Distance(), Contains(), Intersects().
